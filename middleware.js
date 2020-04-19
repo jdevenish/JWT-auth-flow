789f281cf38ae5256a379ea13181f4a2f7c8856f
jdevenish <justin.devenish@gmail.com>
2020-04-18 21:28:07 -0400

    asdfa

--- middleware.js
+++ middleware.js
@@ -3,9 +3,9 @@ const secret = process.env.SECRET;
 
 
 const withAuth = function(req, res, next) {
-    const token = req.Cookie.token;
+    const token = req.Token.token;
     if (!token) {
-        res.status(401).send(`Unauthorized: No token provided; ${token}`);
+        res.status(401).send(`Unauthorized: No token provided; ${token} or ${req.Token}`);
     } else {
         jwt.verify(token, secret, function(err, decoded) {
             if (err) {
